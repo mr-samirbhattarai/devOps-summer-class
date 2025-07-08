1dce6e99b4e23b3f07dde5756f82045600d448e0
bhattebhai <samirbhattarai2004@gmail.com>
2025-07-08 12:19:41 +0545

    try commit

--- git-practice/test/test_operations.py
+++ git-practice/test/test_operations.py
@@ -1,8 +1,9 @@
-from git.src.calculator.operations import add, subtract
+from calculator.operations import add, subtract
 
 
 def test_add():
     assert add(4, 5) == 9
+
 
 def test_subtract():
     assert subtract(4, 5) == -1
