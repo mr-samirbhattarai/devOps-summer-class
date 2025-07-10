f0d18309c3310dd09a342f369b2a654668da3f64
bhattebhai <samirbhattarai2004@gmail.com>
2025-07-10 11:45:14 +0545

    cl.yml file added

--- git-practice/src/calculator/operations.py
+++ git-practice/src/calculator/operations.py
@@ -1,6 +1,13 @@
+# this is code for addition
 def add(a, b):
     return a + b
 
 
+# this is code for subtraction
 def subtract(a, b):
     return a - b
+
+
+# this is code for multiplication
+def multiply(a, b):
+    return a * b
